2968922a1d4bff69506bd16e75d02af8a1ea745a
guimpo <nakaima.prk@gmail.com>
2018-03-14 20:29:04 -0300

    paulo nakaima

---
+++ src/arqsoftware/ArqSoftware.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arqsoftware;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author paulo
+ */
+public class ArqSoftware {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<String> listaNomes = new ArrayList<>();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite o nome:");
+        listaNomes.add(s.nextLine());
+        for()
+        
+    }
+    
+}

6d46f885d3ab5038a021aa1550ae52d7ca03f949
guimpo <nakaima.prk@gmail.com>
2018-03-14 20:50:01 -0300

    Nilton Guilhen

--- src/arqsoftware/ArqSoftware.java
+++ src/arqsoftware/ArqSoftware.java
@@ -6,6 +6,7 @@
 package arqsoftware;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 /**
@@ -19,12 +20,37 @@ public class ArqSoftware {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int aux=0;
+        boolean rodando = true;
+        
         ArrayList<String> listaNomes = new ArrayList<>();
         Scanner s = new Scanner(System.in);
-        System.out.println("Digite o nome:");
-        listaNomes.add(s.nextLine());
-        for()
         
+        System.out.println("1 - cadastrar");
+        System.out.println("2 - listar");
+        System.out.println("3 - sair");
+        System.out.println("Digite sua opção:");
+
+
+        while(rodando){
+            
+        aux = s.nextInt();
+        
+        switch(aux){
+            case 1:
+                    System.out.println("Digite o nome:");
+                    listaNomes.add(s.nextLine());
+                    break;
+            case 2:
+                    for (Iterator<String> it = listaNomes.iterator(); it.hasNext();) {
+                        System.out.println(it.next());
+                    }
+                    break;
+            case 3:
+                    rodando = false;
+                    break;
+        }
+
+        }
     }
-    
 }
